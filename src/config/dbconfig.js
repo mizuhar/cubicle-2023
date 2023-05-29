859ac5efea238479bfdc13b0a44c6f3eddb76134
mizuhar <mizuhar@abv.bg>
2023-05-29 20:33:07 +0300

    Setup mongoose

---
+++ src/config/dbconfig.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const uri = 'mongodb://127.0.0.1:27017/cubicle-2023';
+async function dbConnect() {
+  await mongoose.connect(uri)
+} 
+
+module.exports = dbConnect;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dbConnect = require("./config/dbconfig");
 
 const expressConfig = require("./config/expressCongig");
 const handlebarsConfig = require("./config/handlebarsConfig");
@@ -9,8 +11,12 @@ const PORT = 5000;
 const app = express();
 expressConfig(app);
 handlebarsConfig(app);
+
+dbConnect()
+  .then(() => console.log("DB Connecting succesfully!"))
+  .catch(err => console.log("DB error: ", err));
+
 app.use(routes);
-//app.get('/',homeController.getHome);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}... `);
