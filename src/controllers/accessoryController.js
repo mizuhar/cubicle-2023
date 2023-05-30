d1e12a23f2834709f87983601889d2b6478b3eff
mizuhar <mizuhar@abv.bg>
2023-05-30 19:52:44 +0300

    Add post action to create accessory

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -5,6 +5,11 @@ router.get("/create", (req, res) => {
 
     res.render("accessory/create");
   });
+  router.post("/create", (req, res) => {
+     const body = req.body
+     
+     res.redirect("/");
+  });
 
 
 
