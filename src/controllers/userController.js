ce8452bca969ab9278d1dc6e1db7921bfc6eadaf
mizuhar <mizuhar@abv.bg>
2023-06-06 08:41:30 +0300

    Add register page

---
+++ src/controllers/userController.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+
+
+router.get('/register',(req,res)=>{
+    res.render('users/register')
+
+})
+router.get('/login',(req,res)=>{
+    res.render('users/login')
+
+})
+
+
+
+
+module.exports = router;
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 const routes = require('express').Router();
 const cubeController = require('./controllers/cubeController');
 const homeController = require('./controllers/homeController');
-accessoryController = require('./controllers/accessoryController');
+const accessoryController = require('./controllers/accessoryController');
+const userController = require('./controllers/userController');
 //const routes = express()
 
 routes.use(homeController)
 routes.use('/cubes',cubeController)
 routes.use('/accessory',accessoryController)
+routes.use('/users',userController)
 
 routes.get('*',(req,res)=>{
     res.redirect('/404')
